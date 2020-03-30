0c9cfd3a5443bf5ea1e6b8658a82c20ec09441f5
Kiefer Ragay <kiefer.m.ragay@gmail.com>
2020-03-30 12:00:10 -0700

    Move treasureChest Property to Constructor in ForagerBee.js

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -2,11 +2,12 @@ var ForagerBee = function() {
   Bee.call(this);
   this.age = 10;
   this.job = "find pollen";
+  this.treasureChest = [];
 };
 
 ForagerBee.prototype = Object.create(Bee.prototype);
 ForagerBee.prototype.canFly = true;
-ForagerBee.prototype.treasureChest = [];
+
 ForagerBee.prototype.forage = function(treasure) {
   this.treasureChest.push(treasure);
 }
