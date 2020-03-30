974edb44dc12304e38afb9411ce1709297721abc
Kiefer Ragay <kiefer.m.ragay@gmail.com>
2020-03-30 11:44:49 -0700

    Create ForagerBee Class

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,14 @@
 var ForagerBee = function() {
+  Bee.call(this);
+  this.age = 10;
+  this.job = "find pollen";
 };
+
+ForagerBee.prototype = Object.create(Bee.prototype);
+ForagerBee.prototype.canFly = true;
+ForagerBee.prototype.treasureChest = [];
+ForagerBee.prototype.forage = function(treasure) {
+  this.treasureChest.push(treasure);
+}
+ForagerBee.prototype.constructor = ForagerBee;
 
